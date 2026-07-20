94f4a289ab4d19ad31ccc73cb3bd3e153a069bc8
Luan Vedovoto <vedovotoluan2@gmail.com>
2026-07-20 20:53:32 -0300

    User entity and Resource

---
+++ src/main/java/com/educandoweb/courseprojects/CourseProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.courseprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseProjectsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseProjectsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/educandoweb/courseprojects/CourseProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.courseprojects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseProjectsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
