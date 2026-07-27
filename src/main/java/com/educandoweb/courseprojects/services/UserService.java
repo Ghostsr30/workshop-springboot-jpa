373e070811cfc94f840f3b00e75f018eb73e9593
Luan Vedovoto <vedovotoluan2@gmail.com>
2026-07-27 20:47:56 -0300

    User update

--- src/main/java/com/educandoweb/courseprojects/services/UserService.java
+++ src/main/java/com/educandoweb/courseprojects/services/UserService.java
@@ -2,6 +2,7 @@ package com.educandoweb.courseprojects.services;
 
 import com.educandoweb.courseprojects.entities.User;
 import com.educandoweb.courseprojects.repositories.UserRepository;
+import org.hibernate.sql.Update;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -29,5 +30,17 @@ public class UserService {
 
     public void delete(Long id){
         repository.deleteById(id);
+    }
+
+    public User userUpdate(Long id, User obj){
+        User entity = repository.getReferenceById(id);
+        updateData(entity, obj);
+        return repository.save(entity);
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
     }
 }
